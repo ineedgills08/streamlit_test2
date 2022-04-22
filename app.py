42a8ec31fe8ee6cedc98f6fb11be27ccae358889
Sb Fuller <sbfullerstudio@gmail.com>
2022-04-22 14:29:53 -0400

    first streamlit commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import plotly.express as px
+import pickle
+import numpy as np
+import time 
+from predict import predict_flower
+
+st.title("Iris Species Predictor")
+st.header('Lets predict Irisssssssssssssss')
+st.subheader('Click Below If You Wanna See That DAtaaa')
+
+@st.cache()
+def read_data():
+    return px.data.iris()
+
+
+show_df = st.checkbox("Do you want to see the data?")
+
+df_iris = read_data()
+
+fig = px.histogram(df_iris, x='sepal_length')
+fig
+
+if show_df:
+    df_iris
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    sl = st.number_input("Sepal Length (cm)", 0.0, 10.0)
+with col2:
+    sw = st.number_input("Sepal Width (cm)", 0.0, 100.0)
+with col3:
+    pl = st.number_input("Petal Length (cm)", 0.0, 100.0)
+    pw = st.number_input("Petal Width (cm)", 0.0, 100.0)
+
+user_input = np.array([[sl,sw,pl,pw]])
+
+with open('saved-iris-model.pkl', 'rb') as f:
+    classifier = pickle.load(f)
+
+with st.spinner('Predicting...'):
+    prediction = predict_flower(classifier, user_input)
+
+prediction
+
+
